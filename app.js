e22ef61cf9d3c7d5c7062d23491152d34abdbbdf
Carlos del Prado <carlos.delprado@11paths.com>
2018-06-23 11:23:15 +0200

    feat: db connection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 
+require('./configs/db.config');
+
 app.listen(PORT, () => {
     console.info(`App connected to ${PORT} port.`);
 });
---
+++ configs/db.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const DB_NAME = 'mongoose-basics';
+const MONGO_URI = `mongodb://localhost/${DB_NAME}`;
+mongoose.connect(MONGO_URI);
+mongoose.connection.on('connected', () => {
+    console.info(`Connected to database: ${MONGO_URI}`);
+});
+mongoose.connection.on('error', (error) => {
+    console.error('Database connection error:', error);
+});
